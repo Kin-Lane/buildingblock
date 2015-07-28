5246ee1887c6522d5d51d4508c61eed2ca5ab299
Kin Lane <info@apievangelist.com>
2015-07-28 12:49:42 -0700

    debug

--- api/methods/m-buildingblocks-tags-tag-get.php
+++ api/methods/m-buildingblocks-tags-tag-get.php
@@ -4,21 +4,21 @@ $app->get($route, function ($tag)  use ($app){
 
 	$ReturnObject = array();
 
- 	$request = $app->request(); 
- 	$params = $request->params();	
+ 	$request = $app->request();
+ 	$params = $request->params();
 
-	$tag = trim(mysql_real_escape_string($tag)); 
+	$tag = trim(mysql_real_escape_string($tag));
 
 	$SearchQuery = "SELECT b.Building_Block_ID,b.Building_Block_Category_ID,b.Name,b.About,b.Sort_Order,bbc.Name AS Category,bbc.Type as Type FROM building_block b";
 	$SearchQuery .= " JOIN building_block_category bbc ON b.Building_Block_Category_ID = bbc.BuildingBlockCategory_ID";
 	$SearchQuery .= " JOIN building_block_tag_pivot bbtp ON b.Building_Block_ID = bbtp.Building_Block_ID";
 	$SearchQuery .= " JOIN tags t ON bbtp.Tag_ID = t.Tag_ID";
-	$SearchQuery .= " WHERE t.Tag = '" . $tag . "'";		
+	$SearchQuery .= " WHERE t.Tag = '" . $tag . "'";
 	$SearchQuery .= " ORDER BY b.Sort_Order ASC";
-	//echo $SearchQuery . "<br />";
-	
+	echo $SearchQuery . "<br />";
+
 	$DatabaseResult = mysql_query($SearchQuery) or die('Query failed: ' . mysql_error());
-	  
+
 	while ($Database = mysql_fetch_assoc($DatabaseResult))
 		{
 
@@ -28,7 +28,7 @@ $app->get($route, function ($tag)  use ($app){
 		$about = $Database['About'];
 		$category = $Database['Category'];
 		$category_id = $Database['Building_Block_Category_ID'];
-		$sort_order = $Database['Sort_Order'];	
+		$sort_order = $Database['Sort_Order'];
 
 		$image_path = "";
 		$image_width = "100";
@@ -39,10 +39,10 @@ $app->get($route, function ($tag)  use ($app){
 		while ($row = mysql_fetch_assoc($imageResult))
 			{
 			$image_path = $row['Image_Path'];
-			$image_width = $row['Width'];	
-			if($image_width==''){ $image_width = 100; }						
+			$image_width = $row['Width'];
+			if($image_width==''){ $image_width = 100; }
 			}
-				
+
 		// manipulation zone
 		$host = $_SERVER['HTTP_HOST'];
 		$building_block_id = prepareIdOut($building_block_id,$host);
@@ -58,7 +58,7 @@ $app->get($route, function ($tag)  use ($app){
 		$F['image'] = $image_path;
 		$F['image_width'] = $image_width;
 		$F['sort_order'] = $sort_order;
-		
+
 		array_push($ReturnObject, $F);
 		}
 

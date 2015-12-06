12b6c9138efbf778932b77fa38e100c5eb0a868f
Kin Lane <info@apievangelist.com>
2015-12-06 11:27:11 -0800

    debug

--- api/methods/m-building-blocks-subway-get.php
+++ api/methods/m-building-blocks-subway-get.php
@@ -32,6 +32,7 @@ $app->get($route, function ()  use ($app,$contentType,$githuborg,$githubrepo){
 		$ReturnObject['rel'] = "urn:x-resource:schema:http://kin-lane.github.io/buildingblock/schemas/line.json";
 
     $SearchQuery = "SELECT bbc.Type,bbc.Image,bbc.Hex,bbc.Sort_Order,bbc.Sort_Order_2 FROM building_block_category bbc";
+    $SearchQuery .= " WHERE Sort_Order_2 > 0";
 		$SearchQuery .= " ORDER BY Sort_Order_2 ASC";
 		//echo $SearchQuery . "<br />";
 		$DatabaseResult = mysql_query($SearchQuery) or die('Query failed: ' . mysql_error());
